8fac01fa2c7931a9a082e2f1ee5e4c47e6ee9d5b
S2jieun <oh6318@naver.com>
2022-06-15 14:01:40 +0900

    ts

--- src/main/java/com/tp/yogioteur/config/ServiceConfig.java
+++ src/main/java/com/tp/yogioteur/config/ServiceConfig.java
@@ -1,10 +1,6 @@
 package com.tp.yogioteur.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.goodee.ex11.service.EmployeeService;
-import com.goodee.ex11.service.EmployeeServiceImpl;
 
 @Configuration
 public class ServiceConfig {
