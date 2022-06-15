568528a756f6a3139da4bbee92201e062210bee8
S2jieun <oh6318@naver.com>
2022-06-15 14:23:49 +0900

    text update1

---
+++ src/main/java/com/tp/yogioteur/domain/MemberDTO.java
@@ -0,0 +1,19 @@
+package com.tp.yogioteur.domain;
+
+import java.sql.Date;
+
+public class MemberDTO {
+	
+	private Long memberNo;
+	private String memberName;
+	private String memberEmail;
+	private String memberId;
+	private String memberPw;
+	private Integer memberPhone;
+	private Integer agreeState;
+	private Date memberDate;
+	private String memberSessionId;
+	private Integer memberBirth;
+
+	
+}
