4fec35ead6f3bc375ad1f5b562062cc028895520
easy <tenimyu01@naver.com>
2022-06-23 13:07:15 +0900

    mainpage

--- src/main/java/com/tp/yogioteur/controller/RoomController.java
+++ src/main/java/com/tp/yogioteur/controller/RoomController.java
@@ -1,16 +1,12 @@
 package com.tp.yogioteur.controller;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
-import com.tp.yogioteur.service.ReservationService;
 import com.tp.yogioteur.service.RoomService;
 
 @Controller
